6b4d7a6fe16184d6ca2d92a0efb5d4dacb5925d3
sharon-ku <71299609+sharon-ku@users.noreply.github.com>
2022-05-13 16:06:05 -0400

    SpecPlay2022: Created a container filled with random emojis; added grapheme-splitter.js and jquery libraries

--- js/script.js
+++ js/script.js
@@ -7,6 +7,27 @@ Description will go here
 
 "use strict";
 
+let keyboardEmojis = `🎑🌄🥻🙉🍅🥽🧶👮‍♀️🙊🤞👩‍👧‍👦⚽️👠🧐🧥💂👩‍🦱🌌🐣⌚️👙😉🐗😞🤛🐨🩰🖕👩‍👦👎😒😕😊🌉🚗👉👐🐽🥳🥑👕🐌🏉🩳🕍🚄🚌🍑⛪️✍️🧵🧳🧑😔🐯🏑`;
+// let keyboardEmojis = `🎑🌄🥻🙉🍅👩‍👧‍👦`;
+
+// Grapheme Splitter is a library that handles splitting emojis properly
+// Without it, emojis like this one 👩‍👧‍👦 will count as several emojis
+let splitter = new GraphemeSplitter();
+
+// Split two-char emojis and six-char combined emoji
+let splitEmojis = splitter.splitGraphemes(keyboardEmojis);
+
+// Append emojis to the emoji keyboard
+for (let i = 0; i < splitEmojis.length; i++) {
+  $(`#emoji-keyboard`).append(
+    `<div class="emoji-character">${splitEmojis[i]}</div>`
+  );
+}
+
+/*------------------------
+p5 below - unused for now
+-------------------------*/
+
 /**
 Description of preload
 */
