a63cc19545b4a5077cd621b2c9f51565a841747f
sharon-ku <71299609+sharon-ku@users.noreply.github.com>
2022-06-16 15:47:26 -0400

    SpecPlay2022: added more emoji categories
    To make randomized NPC responses showcase more emojis of the same ideas, I created more categories beyond "positive" and "negative."

--- js/script.js
+++ js/script.js
@@ -11,32 +11,62 @@ Description will go here
 let keyboardEmojis = `🎑🌄🥻🙉🍅🥽🧶👮‍♀️🙊🤞👩‍👧‍👦⚽️👠🧐🧥💂👩‍🦱🌌🐣⌚️👙😉🐗😞🤛🐨🩰🖕👩‍👦👎😒`;
 // let keyboardEmojis = `🎑🌄🥻🙉🍅👩‍👧‍👦`;
 
+// /*
+// Handle NPC facial expressions
+// */
+// // Emojis from: https://www.freecodecamp.org/news/all-emojis-emoji-list-for-copy-and-paste/
+// let smileyEmojis = `🙂😀😄😁😅😆🤣😂🙃😉😊😇😎🤓🧐🥳🤗🤭🤫`;
+// let emotionalEmojis = `🥰😍🤩😘😗😚😙🤔`;
+// let tongueEmojis = `😋😛😜🤪😝🤑`;
+// let neutralEmojis = `😐🤐🤨😑😶😏😒🙄😬‍🤥`;
+// let sleepyEmojis = `😪😴😌😔🤤`;
+// let sickEmojis = `😷🤒🤕🤢🤮🤧🥵🥶🥴😵🤯`;
+// let concernedEmojis = `😕😟🙁😮😯😲😳🥺😦😧😨😰😥😢😭😱😖😞😓😩😫🥱`;
+// let badEmojis = `😤😡😠🤬😈👿💀`;
+//
+// // Temporarily categorizing emojis for sake of simplicity:
+// let positiveEmojis = smileyEmojis + emotionalEmojis + tongueEmojis;
+// let negativeEmojis =
+//   neutralEmojis + sleepyEmojis + sickEmojis + concernedEmojis + badEmojis;
+//
+// // Grapheme Splitter is a library that handles splitting emojis properly
+// // Without it, emojis like this one 👩‍👧‍👦 will count as several emojis
+// let splitter = new GraphemeSplitter();
+//
+// // Split two-char emojis and six-char combined emoji into arrays
+// let positiveEmojisArray = splitter.splitGraphemes(positiveEmojis);
+// let negativeEmojisArray = splitter.splitGraphemes(negativeEmojis);
+
 /*
 Handle NPC facial expressions
 */
 // Emojis from: https://www.freecodecamp.org/news/all-emojis-emoji-list-for-copy-and-paste/
-let smileyEmojis = `🙂😀😄😁😅😆🤣😂🙃😉😊😇😎🤓🧐🥳🤗🤭🤫`;
-let emotionalEmojis = `🥰😍🤩😘😗😚😙🤔`;
-let tongueEmojis = `😋😛😜🤪😝🤑`;
-let neutralEmojis = `😐🤐🤨😑😶😏😒🙄😬‍🤥`;
-let sleepyEmojis = `😪😴😌😔🤤`;
-let sickEmojis = `😷🤒🤕🤢🤮🤧🥵🥶🥴😵🤯`;
-let concernedEmojis = `😕😟🙁😮😯😲😳🥺😦😧😨😰😥😢😭😱😖😞😓😩😫🥱`;
-let badEmojis = `😤😡😠🤬😈👿💀`;
-
-// Temporarily categorizing emojis for sake of simplicity:
-let positiveEmojis = smileyEmojis + emotionalEmojis + tongueEmojis;
-let negativeEmojis =
-  neutralEmojis + sleepyEmojis + sickEmojis + concernedEmojis + badEmojis;
+let hornyEmojis = `😏😘😍😚😙😗😉🤭🤫🤤🤣`;
+let inLoveEmojis = `🥰🤩😇😊🤗😳😎🤓🧐🤑`;
+let happyEmojis = `🙂😀😄😁🥳😋😛😜🤪😝😆😂`;
+let neutralEmojis = `🙃😐😑😶😬`;
+let surprisedEmojis = `😮😯😲🤯😦😧😨😰😱`;
+let boredEmojis = `😪😴🥱😅🤨😒🙄😌😔🤔`;
+let sadEmojis = `😕😟🙁😥😢😭😓😩😫😖😞🥺`;
+let sickEmojis = `😷🤒🤕🤢🤮🤧🥵🥶🥴😵`;
+let angryEmojis = `😤😡😠🤬😈👿💀`;
+let unusedEmojis = `🤐‍🤥`;
 
 // Grapheme Splitter is a library that handles splitting emojis properly
 // Without it, emojis like this one 👩‍👧‍👦 will count as several emojis
-let positiveEmojiSplitter = new GraphemeSplitter();
-let negativeEmojiSplitter = new GraphemeSplitter();
+let splitter = new GraphemeSplitter();
 
 // Split two-char emojis and six-char combined emoji into arrays
-let positiveEmojisArray = positiveEmojiSplitter.splitGraphemes(positiveEmojis);
-let negativeEmojisArray = negativeEmojiSplitter.splitGraphemes(negativeEmojis);
+let numEmojiCategories = 9;
+let hornyEmojisArray = splitter.splitGraphemes(hornyEmojis);
+let inLoveEmojisArray = splitter.splitGraphemes(inLoveEmojis);
+let happyEmojisArray = splitter.splitGraphemes(happyEmojis);
+let neutralEmojisArray = splitter.splitGraphemes(neutralEmojis);
+let surprisedEmojisArray = splitter.splitGraphemes(surprisedEmojis);
+let boredEmojisArray = splitter.splitGraphemes(boredEmojis);
+let sadEmojisArray = splitter.splitGraphemes(sadEmojis);
+let sickEmojisArray = splitter.splitGraphemes(sickEmojis);
+let angryEmojisArray = splitter.splitGraphemes(angryEmojis);
 
 // Odds of getting a positive emoji
 const CHANCE_TO_GET_POSITIVE_EMOJI = 0.5;
@@ -54,11 +84,7 @@ let npcResponseMessage = undefined;
 // Other random emojis to mix in with facial expressions
 let otherEmojis = `💋💖💚💯💦💣💤👋🖖👌🤏🤞🤟🖕👍👎👏🙏💪🦶👀👅🍆🍑🌙🌞⭐🌈🔥✨🎉🏆💩🤡👻👽👑🔪🎈💰🪓🔫`;
 // Split emojis into array
-let otherEmojisArray = positiveEmojiSplitter.splitGraphemes(otherEmojis);
-
-// Grapheme Splitter is a library that handles splitting emojis properly
-// Without it, emojis like this one 👩‍👧‍👦 will count as several emojis
-let splitter = new GraphemeSplitter();
+let otherEmojisArray = splitter.splitGraphemes(otherEmojis);
 
 // Split two-char emojis and six-char combined emoji
 let splitEmojis = splitter.splitGraphemes(keyboardEmojis);
@@ -91,43 +117,87 @@ $(`#send-button`).click(function () {
   npcResponseMessage = ``;
   $(`#npc-response-message`).empty();
 
-  // Randomly update the facial expression of NPC
-  if (Math.random() < CHANCE_TO_GET_POSITIVE_EMOJI) {
-    responseType = `positive`;
-  } else {
-    responseType = `negative`;
-  }
+  // // Randomly update the facial expression of NPC
+  // if (Math.random() < CHANCE_TO_GET_POSITIVE_EMOJI) {
+  //   responseType = `positive`;
+  // } else {
+  //   responseType = `negative`;
+  // }
+  //
+  // // Set response based on emotion
+  // if (responseType === `positive`) {
+  //   // Fetch a positive emoji
+  //   nextEmojiToDisplay = random(positiveEmojisArray);
+  //
+  //   // Compose a positive response message
+  //   composeAMessage(positiveEmojisArray);
+  //
+  //   console.log(npcResponseMessage);
+  // } else if (responseType === `negative`) {
+  //   // Fetch a negative emoji
+  //   nextEmojiToDisplay = random(negativeEmojisArray);
+  //
+  //   // Compose a negative response message
+  //   composeAMessage(negativeEmojisArray);
+  //
+  //   console.log(npcResponseMessage);
+  // }
+  //
+  // console.log(nextEmojiToDisplay);
 
+  // Randomly update the facial expression of NPC
+  let emojiCategories = [
+    `horny`,
+    `inLove`,
+    `happy`,
+    `neutral`,
+    `surprised`,
+    `bored`,
+    `sad`,
+    `sick`,
+    `angry`,
+  ];
+  let randomIndex = Math.floor(Math.random() * emojiCategories.length);
+  let responseType = emojiCategories[randomIndex];
   console.log(responseType);
 
-  // Set response based on emotion
-  if (responseType === `positive`) {
-    // Fetch a positive emoji
-    nextEmojiToDisplay = random(positiveEmojisArray);
+  // PUT CODE HERE
 
-    // Compose a positive response message
-    composeAMessage(positiveEmojisArray);
-
-    console.log(npcResponseMessage);
-  } else if (responseType === `negative`) {
-    // Fetch a negative emoji
-    nextEmojiToDisplay = random(negativeEmojisArray);
-
-    // Compose a negative response message
-    composeAMessage(negativeEmojisArray);
-
-    console.log(npcResponseMessage);
+  if (responseType === `horny`) {
+    setNpcReaction(hornyEmojisArray);
+  } else if (responseType === `inLove`) {
+    setNpcReaction(inLoveEmojisArray);
+  } else if (responseType === `happy`) {
+    setNpcReaction(happyEmojisArray);
+  } else if (responseType === `neutral`) {
+    setNpcReaction(neutralEmojisArray);
+  } else if (responseType === `surprised`) {
+    setNpcReaction(surprisedEmojisArray);
+  } else if (responseType === `bored`) {
+    setNpcReaction(boredEmojisArray);
+  } else if (responseType === `sad`) {
+    setNpcReaction(sadEmojisArray);
+  } else if (responseType === `sick`) {
+    setNpcReaction(sickEmojisArray);
+  } else if (responseType === `angry`) {
+    setNpcReaction(angryEmojisArray);
   }
-
-  console.log(nextEmojiToDisplay);
 
   $(`#emoji-face`).text(nextEmojiToDisplay);
   $(`#npc-response-message`).text(npcResponseMessage);
 });
 
+function setNpcReaction(reactionArray) {
+  // Fetch a positive emoji
+  nextEmojiToDisplay = random(reactionArray);
+
+  // Compose a positive response message
+  composeAMessage(reactionArray);
+}
+
 function composeAMessage(emojiArraySet) {
   // Compose a positive response message
-  for (let i = 0; i < Math.ceil(Math.random() * 4); i++) {
+  for (let i = 0; i < Math.ceil(Math.random() * 3); i++) {
     let newCharacter;
     if (Math.random() < 0.2) {
       newCharacter = random(emojiArraySet);
